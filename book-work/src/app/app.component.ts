82b049e8fdaa61062fcb453adf0541b5fd6598c6
Pat Murphy <murphy4414@gmail.com>
2019-02-12 19:24:29 -0500

    Updated angular.json with scss extension, created data-binding component

--- book-work/src/app/app.component.ts
+++ book-work/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'book-work';
+  title = 'Various small projects from Angular book';
+  subTitle = 'Topics spanning interpolation and other issues'
 }
